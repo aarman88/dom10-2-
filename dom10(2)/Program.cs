2b6106b0492048a392fe1c0d738b2636d3269534
Арман <@ARMAN>
2023-11-12 15:45:47 +0600

    Добавьте файлы проекта.

---
+++ dom10(2)/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+public class SimpleCalculator : ICalculatable
+{
+    public double Add(double a, double b)
+    {
+        return a + b;
+    }
+
+    public double Subtract(double a, double b)
+    {
+        return a - b;
+    }
+
+    public double Multiply(double a, double b)
+    {
+        return a * b;
+    }
+
+    public double Divide(double a, double b)
+    {
+        if (b == 0)
+        {
+            throw new ArgumentException("Деление на ноль недопустимо.");
+        }
+        return a / b;
+    }
+
+    public void PerformCalculation()
+    {
+        Console.WriteLine("Выберите операцию:");
+        Console.WriteLine("1. Сложение");
+        Console.WriteLine("2. Вычитание");
+        Console.WriteLine("3. Умножение");
+        Console.WriteLine("4. Деление");
+
+        int choice = int.Parse(Console.ReadLine());
+
+        Console.Write("Введите первое число: ");
+        double num1 = double.Parse(Console.ReadLine());
+
+        Console.Write("Введите второе число: ");
+        double num2 = double.Parse(Console.ReadLine());
+
+        double result = 0;
+        switch (choice)
+        {
+            case 1:
+                result = Add(num1, num2);
+                break;
+            case 2:
+                result = Subtract(num1, num2);
+                break;
+            case 3:
+                result = Multiply(num1, num2);
+                break;
+            case 4:
+                result = Divide(num1, num2);
+                break;
+            default:
+                Console.WriteLine("Неверный выбор операции.");
+                return;
+        }
+
+        Console.WriteLine($"Результат операции: {result}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        SimpleCalculator calculator = new SimpleCalculator();
+        calculator.PerformCalculation();
+    }
+}
